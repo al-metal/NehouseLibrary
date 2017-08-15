8bba28c74840a3f29782c9087b8e7fc66e5b43a5
al-metal <al-metal@users.noreply.github.com>
2017-08-15 14:20:33 +0400

    Новая библиотека

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NehouseLibrary
